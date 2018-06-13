3703f94c1f9a2e0ce142283b04f7e46608bb4fd8
Christer Ljung <cljung@users.noreply.github.com>
2018-06-13 20:29:36 +0200

    if not exists

--- createdb.php
+++ createdb.php
@@ -2,7 +2,7 @@
 //including the database connection file
 include_once("config.php");
 
-$result = mysqli_query($mysqli, "CREATE TABLE `TMessage` (`id` int(11) NOT NULL auto_increment,	`sender` varchar(40) NOT NULL, `msg` varchar(255) NOT NULL, PRIMARY KEY  (`id`) )");
+$result = mysqli_query($mysqli, "CREATE TABLE IF NOT EXISTS `TMessage` (`id` int(11) NOT NULL auto_increment,	`sender` varchar(40) NOT NULL, `msg` varchar(255) NOT NULL, PRIMARY KEY  (`id`) )");
 ?>
 
 <html>
